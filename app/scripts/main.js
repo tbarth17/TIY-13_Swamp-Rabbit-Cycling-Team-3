8d12882be0eaeeaadb24d62f7c7bf8a4d0b1313a
jgoley <jgoley@gmail.com>
2014-09-18 17:54:03 -0400

    Adds jquery hammer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,26 +4,33 @@
 
 function renderTemplate(scriptID, whereTo, data) {
     console.log(data);
-    var template = _.template( $("#" + scriptID).text() );
+    var template = _.template($("#" + scriptID).text());
     $(whereTo).append(template(data));
 
 }
 
 var flickrKey = "0c74aabb810c286e7cb95d06496650f2",
-    tags = "bike,bicycles,mountainbike",
+    tags = "bike,bicycles,mountain,bicycle",
     flickrCount = 3,
     flickrSize = "_z";
 
 var flickrApiLink =
-    "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + flickrKey + "&format=json&tags=" + tags + "&jsoncallback=?&per_page="+flickrCount;
+    "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + flickrKey + "&format=json&tags=" + tags + "&jsoncallback=?&per_page=" + flickrCount;
 $.ajax({
     url: flickrApiLink,
     type: "GET",
     dataType: "json"
 }).done(function(photoData) {
     _.each(photoData.photos.photo, function(img) {
-        var link = {flickrLink: "https://farm" + img.farm + ".staticflickr.com/" + img.server + "/" + img.id + "_" + img.secret + flickrSize +".jpg"};
+        var link = {
+            flickrLink: "https://farm" + img.farm + ".staticflickr.com/" + img.server + "/" + img.id + "_" + img.secret + flickrSize + ".jpg"
+        };
         console.log(link);
-            renderTemplate("template-flikr", ".flikr-content", link);
+        renderTemplate("template-flikr", ".flikr-content", link);
     });
 });
+
+// Hammer JS
+    $(".flikr-content").hammer().bind("pan", function(){
+        console.log("Touched!");
+    });
