84ed0d26d81bfb4cdbae08788dc2b1e42ecbf1d1
jgoley <jgoley@gmail.com>
2014-09-18 15:02:30 -0400

    Fixes ajax request and template function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,14 +3,16 @@
 // 5c3bc5492969afe0
 
 function renderTemplate(scriptID, whereTo, data) {
-    var template = _.template($("#" + scriptID.text()));
+    console.log(data);
+    var template = _.template( $("#" + scriptID).text() );
     $(whereTo).append(template(data));
 
 }
 
 var flickrKey = "0c74aabb810c286e7cb95d06496650f2",
     tags = "bike,bicycles,mountainbike",
-    flickrCount = 3;
+    flickrCount = 3,
+    flickrSize = "_z";
 
 var flickrApiLink =
     "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + flickrKey + "&format=json&tags=" + tags + "&jsoncallback=?&per_page="+flickrCount;
@@ -20,7 +22,8 @@ $.ajax({
     dataType: "json"
 }).done(function(photoData) {
     _.each(photoData.photos.photo, function(img) {
-        var link = "https://farm" + img.farm + ".staticflickr.com/" + img.server + "/" + img.id + "_" + img.secret + "_s.jpg";
+        var link = {flickrLink: "https://farm" + img.farm + ".staticflickr.com/" + img.server + "/" + img.id + "_" + img.secret + flickrSize +".jpg"};
         console.log(link);
+            renderTemplate("template-flikr", ".flikr-content", link);
     });
 });
